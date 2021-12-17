fc9a57d7cbd1d67df93119d882c93bc99137182a
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-17 12:01:47 +0000

    added exercise model with validations, added exercises table with columns, seeded database with 3 exercises

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+exercise_1 = Exercise.new(name: "Major scale modes, 3nps", description: "Practicing the 3nps shapes of all 7 modes of the major scale, eigth notes at the given tempo", user_id: 1)
+exercise_1.save!
+
+exercise_2 = Exercise.new(name: "Trills", description: "Trills off first finger for 2nd, 3rd and 4th fingers", user_id: 1)
+exercise_2.save!
+
+exercise_3 = Exercise.new(name: "Tremolo picking", description: "Tremolo picking on all strings, fretted at 5", user_id: 1)
+exercise_3.save!
