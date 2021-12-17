761a67aa00cfbb760ac00acbbca9abae8b294ec6
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-17 16:20:30 +0000

    working new exercise form

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -6,4 +6,20 @@ class ExercisesController < ApplicationController
   def new
     @exercise = Exercise.new
   end
+
+  def create
+    @exercise = Exercise.new(exercise_params)
+    @exercise.user = current_user
+    if @exercise.save
+      redirect_to exercises_path
+    else
+      render[:new]
+    end
+  end
+
+  private
+
+  def exercise_params
+    params.require(:exercise).permit(:name, :description)
+  end
 end
