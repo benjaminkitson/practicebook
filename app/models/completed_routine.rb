5831d3ef5b28f986063eeb9d25ef8cffdf19eab4
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-18 12:50:35 +0000

    added has_manys to models

--- app/models/completed_routine.rb
+++ app/models/completed_routine.rb
@@ -1,3 +1,4 @@
 class CompletedRoutine < ApplicationRecord
   belongs_to :user
+  has_many :routine_exercises
 end
--- app/models/routine_template.rb
+++ app/models/routine_template.rb
@@ -1,3 +1,5 @@
 class RoutineTemplate < ApplicationRecord
+  validates :name, presence: true
   belongs_to :user
+  has_many :routine_exercises
 end
