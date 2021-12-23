7ab34839795e5343c6add3f780af861ece5008e8
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-23 22:45:21 +0000

    data from list now stored in formdata in approximately correct format

--- app/javascript/components/add_exercise.js
+++ app/javascript/components/add_exercise.js
@@ -6,7 +6,7 @@ const addExercise = () => {
   function test() {
     const name = this.getAttribute("data-name")
     const description = this.getAttribute("data-description")
-    const html = `<div class=\"item\" data-e-name=\"${name}\" data-e-description=\"${description}\"><div class=\"exercise-index-preview\">${name}</div><div class=\"item-description\">${description}</div></div>`
+    const html = `<div class=\"ajax item\" data-e-name=\"${name}\" data-e-description=\"${description}\"><div class=\"exercise-index-preview\">${name}</div><div class=\"item-description\">${description}</div></div>`
     exercisesContainer.insertAdjacentHTML('beforeend', html)
   };
 
--- app/javascript/components/routine_ajax.js
+++ app/javascript/components/routine_ajax.js
@@ -1,14 +1,39 @@
 const routineAjax = () => {
 
-  function test(e) {
-    e.preventDefault();
-    console.log("hello")
+  const createButton = document.querySelector('.create-button');
+  const createForm = document.getElementById('new_routine_template');
+
+  function test() {
+
   }
 
-  const createButton = document.querySelector('.create-button');
+  function send(e) {
+    e.preventDefault();
+    const exerciseTexts = document.querySelectorAll('.ajax');
+    const ajaxArray = []
 
-  createButton.addEventListener('click', test)
+    exerciseTexts.forEach(exerciseText => {
+      ajaxArray.push([["routine_exercise[name]", exerciseText.getAttribute("data-e-name")], ["routine_exercise[description]", exerciseText.getAttribute("data-e-description")]])
+    })
 
+    console.log(ajaxArray)
+    let form = new FormData(createForm)
+    for (const entry of form.entries()) {
+      console.log(entry);
+    }
+    // fetch(this.formTarget.action, {
+    //   method: 'POST',
+    //   headers: { 'Accept': "application/json", 'X-CSRF-Token': csrfToken() },
+    //   body: new FormData(this.formTarget)
+    // })
+    //   .then(response => response.json())
+    //   .then((data) => {
+    //     console.log(data)
+    //   });
+  }
+
+  // createButton.addEventListener('click', send)
+  createForm.addEventListener('submit', send)
 
 }
 
