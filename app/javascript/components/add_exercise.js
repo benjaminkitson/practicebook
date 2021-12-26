df8d0accb5f74f1ec7cb6758fb93701708e96e2e
Benjamin Kitson <benjaminkitson13@gmail.com>
2021-12-26 14:41:23 +0000

    commented out potentially redundant javascript files, trying a controller-oriented approach

--- app/javascript/components/add_exercise.js
+++ app/javascript/components/add_exercise.js
@@ -1,19 +1,19 @@
-const addExercise = () => {
+// const addExercise = () => {
 
-  const exercisesContainer = document.querySelector('.routine-exercises-container');
-  const plusButtons = document.querySelectorAll('.add-item');
+//   const exercisesContainer = document.querySelector('.routine-exercises-container');
+//   const plusButtons = document.querySelectorAll('.add-item');
 
-  function test() {
-    const name = this.getAttribute("data-name")
-    const description = this.getAttribute("data-description")
-    const html = `<div class=\"ajax item\" data-e-name=\"${name}\" data-e-description=\"${description}\"><div class=\"exercise-index-preview\">${name}</div><div class=\"item-description\">${description}</div></div>`
-    exercisesContainer.insertAdjacentHTML('beforeend', html)
-  };
+//   function test() {
+//     const name = this.getAttribute("data-name")
+//     const description = this.getAttribute("data-description")
+//     const html = `<div class=\"ajax item\" data-e-name=\"${name}\" data-e-description=\"${description}\"><div class=\"exercise-index-preview\">${name}</div><div class=\"item-description\">${description}</div></div>`
+//     exercisesContainer.insertAdjacentHTML('beforeend', html)
+//   };
 
-  plusButtons.forEach( plusButton => {
-    plusButton.addEventListener('click', test);
-  });
+//   plusButtons.forEach( plusButton => {
+//     plusButton.addEventListener('click', test);
+//   });
 
-}
+// }
 
-export { addExercise };
+// export { addExercise };
