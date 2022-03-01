d9596ca5b2bae102850152963b61753393db2349
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-03-01 10:04:03 +0000

    react-router 4.2 initialised in app - intend to update to v6 later

--- src/app.js
+++ src/app.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './styles/styles.scss';
 
 const app = document.getElementById('app');
 
-ReactDOM.render(<h1>Hello there!</h1>, app);
+const Dashboard = () => (<div>Le dashboard</div>);
+
+const routes = (
+  <BrowserRouter>
+    <Route path="/" component={Dashboard}/>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, app);
