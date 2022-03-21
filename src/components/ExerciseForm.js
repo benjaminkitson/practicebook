548fdc117982d307d2fe722243689e6866f0c8c5
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-03-21 11:58:48 +0000

    more app progress

--- src/components/ExerciseForm.js
+++ src/components/ExerciseForm.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import { addExercise } from '../actions/exercises';
 
 export default class ExerciseForm extends React.Component {
+  state = {
+    title: '',
+    duration: ''
+  };
+  onTitleChange = (e) => {
+    const title = e.target.value;
+    this.setState(() => ({ title }));
+  };
+  onDurationChange = (e) => {
+    const duration = e.target.value;
+    if (duration.match(/^\d*/)) {
+      this.setState(() => ({ duration }));
+    }
+  };
   render() {
     return (
       <div>
         <form>
-          <input type="text" placeholder="description" autoFocus />
-          <input type="number" placeholder="duration" />
+          <input type="text" placeholder="title" autoFocus value={this.state.title}
+            onChange={this.onTitleChange}/>
+          <input type="text" placeholder="duration" value={this.state.duration}
+            onChange={this.onDurationChange}/>
+          <button>Add exercise</button>
         </form>
       </div>
     );
