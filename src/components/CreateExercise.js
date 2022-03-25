b0e82b082bc001f2beeb2d5018569fdd9cc5bfc5
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-03-25 14:11:26 +0000

    more progress on app

--- src/components/CreateExercise.js
+++ src/components/CreateExercise.js
@@ -1,6 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { addExercise } from '../actions/exercises';
 import ExerciseForm from './ExerciseForm';
+import { useNavigate } from 'react-router-dom';
 
-const CreateExercise = () => (<div><ExerciseForm/></div>);
+const CreateExercise = (props) => {
 
-export default CreateExercise;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>Create Exercise</h1>
+      <ExerciseForm
+        onSubmit={(exercise) => {
+          props.dispatch(addExercise(exercise));
+          console.log(props);
+          navigate('/');
+        }}
+      />
+    </div>
+  );
+
+};
+
+export default connect()(CreateExercise);
--- src/components/ExerciseForm.js
+++ src/components/ExerciseForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { addExercise } from '../actions/exercises';
+// import { addExercise } from '../actions/exercises';
+import moment from 'moment';
+import 'react-dates/initialize';
+import { SingleDatePicker } from 'react-dates';
+import "react-dates/lib/css/_datepicker.css";
 
 export default class ExerciseForm extends React.Component {
   state = {
     title: '',
-    duration: ''
+    duration: '',
+    createdAt: moment(),
+    calendarFocused: false,
+    error: undefined
   };
   onTitleChange = (e) => {
     const title = e.target.value;
@@ -12,18 +19,49 @@ export default class ExerciseForm extends React.Component {
   };
   onDurationChange = (e) => {
     const duration = e.target.value;
-    if (duration.match(/^\d*/)) {
+    if (!duration || duration.match(/^\d{1,}/)) {
       this.setState(() => ({ duration }));
+    }
+  };
+  onDateChange = (createdAt) => {
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
+  };
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.title || !this.state.duration) {
+      this.setState(() => ({ error: 'Please provide title and duration' }));
+    } else {
+      this.setState(() => ({error: undefined}));
+      console.log(this.state.error);
+      console.log('submitted');
+      this.props.onSubmit({
+        title: this.state.title,
+        duration: parseInt(this.state.duration),
+        createdAt: this.state.createdAt.valueOf()
+      });
     }
   };
   render() {
     return (
       <div>
-        <form>
+        {this.state.error && <p>{this.state.error}</p>}
+        <form onSubmit={this.onSubmit}>
           <input type="text" placeholder="title" autoFocus value={this.state.title}
             onChange={this.onTitleChange}/>
           <input type="text" placeholder="duration" value={this.state.duration}
             onChange={this.onDurationChange}/>
+          <SingleDatePicker date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            id="practicebook_datepicker"
+            numberOfMonths={1}
+            isOutsideRange={() => false}/>
           <button>Add exercise</button>
         </form>
       </div>
