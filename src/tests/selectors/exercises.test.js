ad4a1ddd44d545ec617bb51ced5e92c271e10943
Benjamin Kitson <benjaminkitson13@gmail.com>
2022-03-30 17:57:09 +0100

    added durations to test exercises, created tests for sorting by date and duration

--- src/tests/selectors/exercises.test.js
+++ src/tests/selectors/exercises.test.js
@@ -5,18 +5,21 @@ const exercises = [{
   id: 1,
   title: 'first test',
   description: 'a description',
+  duration: 50,
   createdAt: 0
 },
 {
   id: 2,
   title: 'second test',
   description: 'c description',
+  duration: 10,
   createdAt: moment(0).subtract(4, 'days')
 },
 {
   id: 3,
   title: 'third test',
   description: 'b description',
+  duration: 30,
   createdAt: moment(0).add(4, 'days')
 }];
 
@@ -51,4 +54,26 @@ test('should filter by end date', () => {
   };
   const result = selectExercises(exercises, filters);
   expect(result).toEqual([exercises[1], exercises[0]]);
+});
+
+test('should sort by date', () => {
+  const filters = {
+    text: '',
+    sortBy: 'date',
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExercises(exercises, filters);
+  expect(result).toEqual([exercises[1], exercises[0], exercises[2]]);
+});
+
+test('should sort by duration', () => {
+  const filters = {
+    text: '',
+    sortBy: 'duration',
+    startDate: undefined,
+    endDate: undefined
+  };
+  const result = selectExercises(exercises, filters);
+  expect(result).toEqual([exercises[0], exercises[2], exercises[1]]);
 });
